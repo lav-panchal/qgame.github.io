1b7d7de3885bcbf5bb50b777f3ef7aa1145c00c1
Lav Panchal <35377777+lovepanchal1912@users.noreply.github.com>
2021-04-03 14:47:51 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,590 @@
+$(document).ready(function(){
+
+									//     questions hide-show
+
+	$(".q-1").show();
+	$(".q-2").hide();
+	$(".q-3").hide();
+	$(".q-4").hide();
+	$(".q-5").hide();
+	$(".q-6").hide();
+	$(".q-7").hide();
+	$(".q-8").hide();
+	$(".q-9").hide();
+
+											//  Q-1 Answer   
+
+	$(".img-11").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-12").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-13").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-14").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-15").click(function(){
+		$(".ans-1").removeClass("red");
+		$(".ans-1").addClass("green");
+	});	
+	$(".img-16").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-17").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-18").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});	
+	$(".img-19").click(function(){
+		$(".ans-1").removeClass("green");
+		$(".ans-1").addClass("red");
+	});		
+
+
+												//  Q-2 Answer   
+
+	$(".img-21").click(function(){
+		$(".ans-2").removeClass("red");
+		$(".ans-2").addClass("green");
+	});	
+	$(".img-22").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-23").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-24").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-25").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-26").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-27").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-28").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});	
+	$(".img-29").click(function(){
+		$(".ans-2").removeClass("green");
+		$(".ans-2").addClass("red");
+	});		
+
+
+													//  Q-3 Answer   
+
+	$(".img-31").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-32").click(function(){
+		$(".ans-3").removeClass("red");
+		$(".ans-3").addClass("green");
+	});	
+	$(".img-33").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-34").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-35").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-36").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-37").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-38").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});	
+	$(".img-39").click(function(){
+		$(".ans-3").removeClass("green");
+		$(".ans-3").addClass("red");
+	});		4
+
+												//  Q-4 Answer   
+
+	$(".img-41").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-42").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-43").click(function(){
+		$(".ans-4").removeClass("red");
+		$(".ans-4").addClass("green");
+	});	
+	$(".img-44").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-45").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-46").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-47").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-48").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+	$(".img-49").click(function(){
+		$(".ans-4").removeClass("green");
+		$(".ans-4").addClass("red");
+	});	
+
+
+											//  Q-5 Answer   
+
+	$(".img-51").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-52").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-53").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-54").click(function(){
+		$(".ans-5").removeClass("red");
+		$(".ans-5").addClass("green");
+	});	
+	$(".img-55").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-56").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-57").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-58").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+	$(".img-59").click(function(){
+		$(".ans-5").removeClass("green");
+		$(".ans-5").addClass("red");
+	});	
+
+
+										//  Q-6 Answer   
+
+	$(".img-61").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-62").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-63").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-64").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-65").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-66").click(function(){
+		$(".ans-6").removeClass("red");
+		$(".ans-6").addClass("green");
+	});	
+	$(".img-67").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-68").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+	$(".img-69").click(function(){
+		$(".ans-6").removeClass("green");
+		$(".ans-6").addClass("red");
+	});	
+
+
+									//  Q-7 Answer   
+
+	$(".img-71").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-72").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-73").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-74").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-75").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-76").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-77").click(function(){
+		$(".ans-7").removeClass("red");
+		$(".ans-7").addClass("green");
+	});	
+	$(".img-78").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+	$(".img-79").click(function(){
+		$(".ans-7").removeClass("green");
+		$(".ans-7").addClass("red");
+	});	
+
+
+											//  Q-8 Answer   
+
+	$(".img-81").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-82").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-83").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-84").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-85").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-86").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-87").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+	$(".img-88").click(function(){
+		$(".ans-8").removeClass("red");
+		$(".ans-8").addClass("green");
+	});	
+	$(".img-89").click(function(){
+		$(".ans-8").removeClass("green");
+		$(".ans-8").addClass("red");
+	});	
+
+
+										//  Q-9 Answer   
+
+	$(".img-91").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-92").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-93").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-94").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-95").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-96").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-97").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-98").click(function(){
+		$(".ans-9").removeClass("green");
+		$(".ans-9").addClass("red");
+	});	
+	$(".img-99").click(function(){
+		$(".ans-9").removeClass("red");
+		$(".ans-9").addClass("green");
+	});	
+
+
+
+								//   Next-1
+
+	$(".next-1").click(function(){
+		$(".q-2").show();
+		$(".q-1").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});	
+								//   Next-2
+
+	$(".next-2").click(function(){
+		$(".q-3").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});	
+								//   Next-3
+
+	$(".next-3").click(function(){
+		$(".q-4").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});	
+								//   Next-4
+
+	$(".next-4").click(function(){
+		$(".q-5").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-4").hide();
+		$(".q-3").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});	
+
+								//   Next-5
+
+	$(".next-5").click(function(){
+		$(".q-6").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});	
+								//   Next-6
+
+	$(".next-6").click(function(){
+		$(".q-7").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});	
+
+								//   Next-7
+
+	$(".next-7").click(function(){
+		$(".q-8").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-9").hide();
+	});	
+								//   Next-8
+
+	$(".next-8").click(function(){
+		$(".q-9").show();
+		$(".q-1").hide();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-6").hide();
+	});
+
+
+								//  Pre-2
+
+	$(".pre-2").click(function(){
+		$(".q-1").show();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+
+								//  Pre-3
+
+	$(".pre-3").click(function(){
+		$(".q-2").show();
+		$(".q-1").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+								//  Pre-4
+
+	$(".pre-4").click(function(){
+		$(".q-3").show();
+		$(".q-2").hide();
+		$(".q-1").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+
+								//  Pre-5
+
+	$(".pre-5").click(function(){
+		$(".q-4").show();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-5").hide();
+		$(".q-1").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+								//  Pre-6
+
+	$(".pre-6").click(function(){
+		$(".q-5").show();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-1").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+								//  Pre-7
+
+	$(".pre-7").click(function(){
+		$(".q-6").show();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-1").hide();
+		$(".q-7").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+
+								//  Pre-8
+
+	$(".pre-8").click(function(){
+		$(".q-7").show();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-1").hide();
+		$(".q-8").hide();
+		$(".q-9").hide();
+	});
+								//  Pre-9
+
+	$(".pre-9").click(function(){
+		$(".q-8").show();
+		$(".q-2").hide();
+		$(".q-3").hide();
+		$(".q-4").hide();
+		$(".q-5").hide();
+		$(".q-6").hide();
+		$(".q-7").hide();
+		$(".q-1").hide();
+		$(".q-9").hide();
+	});
+})
